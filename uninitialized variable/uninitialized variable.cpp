da49728b0236cab5079025eda485cf13997a054c
Ali <ali.mtarar@gmail.com>
2024-04-22 11:31:41 +1000

    yes

--- uninitialized variable/uninitialized variable.cpp
+++ uninitialized variable/uninitialized variable.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-void doNothing(int&) {
+void doNothing(int&) { // void function used to bypass Visual Studio limitation on uninitialised variables
 
 }
 
-int main() {
-	int x;
+int main() { 
+	int x; // uninitialised integer variable named x
 	
-	doNothing(x);
+	doNothing(x); // make the compiler think we're assigning a value to this variable
 
-	std::cout << x << '\n';
+	std::cout << x << '\n'; // print the value of x to the screen (not sure what we'll get because x is uninitialised
 
 	return 0;
 }
